9ad9831a2b62bf2fd1d3baf126dd39875cbb28ce
Tom Wark <tom@tomwark.me>
2016-04-25 09:10:10 -0400

    First commit

---
+++ CriminalIntent/app/src/main/java/com/example/tomwark/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package com.example.tomwark.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by tomwark on 16-04-24.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
